6aeca31d1b0c3d2d537bad4f3c603cf52b24f1ac
amir-haghighi <hagh73h@gmail.com>
2024-03-02 06:53:36 -0800

    pwa is ready

---
+++ src/api/useData.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import { useQuery } from "@tanstack/react-query";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "f33a484cf794d08d0148764789aaba32";
+export const useData = (query) => {
+  const axiosFn = (query) => {
+    return axios.get(URL, {
+      params: {
+        q: query,
+        units: "metric",
+        APPID: API_KEY,
+      },
+    });
+  };
+
+  return useQuery({
+    queryKey: ["weatherData"],
+    queryFn: () => {
+      if (!query) {
+        return Promise.reject(new Error("Query cannot be empty")); // Reject for empty queries
+      }
+
+      return axiosFn(query);
+    },
+    enabled: false,
+  });
+};
